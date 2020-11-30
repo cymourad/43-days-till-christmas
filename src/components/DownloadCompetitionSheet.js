1a3ba629c733db32d481957731781f0d07685339
Christian Mourad <christian.y.isk@gmail.com>
2020-11-30 15:34:09 -0500

    lets user download docs competition sheet

--- src/components/DownloadCompetitionSheet.js
+++ src/components/DownloadCompetitionSheet.js
@@ -7,27 +7,48 @@ import React from "react";
 
 import Button from "@material-ui/core/Button";
 
-import adventBibleCompetitionSheet from "../static/advent_competition.pdf";
-import adventTheotokiaCompetitionSheet from "../static/advent_competition_theotokia.pdf";
+import adventBibleCompetitionPDF from "../static/advent_competition.pdf";
+import adventTheotokiaCompetitionPDF from "../static/advent_competition_theotokia.pdf";
+
+import adventBibleCompetitionDOCX from "../static/advent_competition.docx";
+import adventTheotokiaCompetitionDOCX from "../static/advent_competition_theotokia.docx";
 
 const DonwloadCompetitionSheet = ({ isTheotokia }) => {
 	return (
-		<Button
-			style={{ paddingTop: 15 }}
-			color="secondary"
-			href={
-				isTheotokia
-					? adventTheotokiaCompetitionSheet
-					: adventBibleCompetitionSheet
-			}
-			download={
-				isTheotokia
-					? "advent_theotokia_competition.pdf"
-					: "advent_bible_competition.pdf"
-			}
-		>
-			Download Competition Sheet
-		</Button>
+		<div>
+			<Button
+				style={{ paddingTop: 15 }}
+				color="secondary"
+				href={
+					isTheotokia
+						? adventTheotokiaCompetitionPDF
+						: adventBibleCompetitionPDF
+				}
+				download={
+					isTheotokia
+						? "advent_theotokia_competition.pdf"
+						: "advent_bible_competition.pdf"
+				}
+			>
+				Download Competition Sheet (PDF)
+			</Button>
+			<Button
+				style={{ paddingTop: 15 }}
+				color="secondary"
+				href={
+					isTheotokia
+						? adventTheotokiaCompetitionDOCX
+						: adventBibleCompetitionDOCX
+				}
+				download={
+					isTheotokia
+						? "advent_theotokia_competition.docx"
+						: "advent_bible_competition.docx"
+				}
+			>
+				Download Competition Sheet (DOCX)
+			</Button>
+		</div>
 	);
 };
 
